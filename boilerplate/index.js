42aa1d952231044c99423df41ddb3299e3be02f3
Gangatharan Raja <gangatharanjj@gmail.com>
2018-03-14 16:27:08 +0530

    Recent changes on file preview and debug

--- boilerplate/index.js
+++ boilerplate/index.js
@@ -1,7 +1,8 @@
 'use strict';
 const electron = require('electron');
 const app = electron.app;
-require('electron-debug')();
+global.filepreview = require('filepreview');
+//require('electron-debug')();
 let mainWindow;
 
 function onClosed() {
@@ -33,4 +34,10 @@ app.on('activate', () => {
 
 app.on('ready', () => {
 	mainWindow = createMainWindow();
+		filepreview.generate('https://www.google.co.in/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwjh6MLW0u3YAhWBOpQKHULbAFQQFggmMAA&url=http%3A%2F%2Fgahp.net%2Fwp-content%2Fuploads%2F2017%2F09%2Fsample.pdf&usg=AOvVaw1uNIspJe1bqvlT-j0ZbIQn', 'https://www.google.co.in/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwi8ttzq0u3YAhXJG5QKHWQyBf4QjRwIBw&url=http%3A%2F%2Fwww.cameraegg.org%2Fcanon-powershot-g3-x-sample-images%2F&psig=AOvVaw2pktc5SvwGcHpBa5znwVfb&ust=1516781843854287', function(error) {
+		if (error) {
+		  return console.log(error);
+		}
+		console.log('File preview is /home/myfile_preview.gif');
+	});	
 });
--- child-window-parent-window/main.js
+++ child-window-parent-window/main.js
@@ -6,11 +6,32 @@ const BrowserWindow = electron.BrowserWindow;
 var mainWindow;
 
 app.on('ready',function(){
-	mainWindow = new BrowserWindow({width: 1024, height: 768, backgroundColor: '#2e2c29'});
+	mainWindow = new BrowserWindow({
+			width: 1024,
+			height: 768,
+			backgroundColor: '#2e2c29',
+			webPreferences: {
+				nativeWindowOpen: true
+			}
+		});
 	mainWindow.loadURL(url.format({
 		pathname: path.join(__dirname, 'index.html'),
 		protocol: 'file',
 		slashes: true,
 		nativeWindowOpen : true
 	}));
+	
+	mainWindow.webContents.on('new-window', (event, url, frameName, disposition, options, additionalFeatures) => {
+	  if (frameName === 'modal') {
+		// open window as modal
+		event.preventDefault()
+		Object.assign(options, {
+		  modal: true,
+		  parent: mainWindow,
+		  width: 100,
+		  height: 100
+		})
+		event.newGuest = new BrowserWindow(options)
+	  }
+	});
 });
