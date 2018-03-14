8fa68f872b53dc280c6ecdadb1d6719b3812d885
Gangatharan Raja <gangatharanjj@gmail.com>
2018-03-14 16:31:29 +0530

    Recent changes on file preview

---
+++ electron-crash-report/index.js
@@ -0,0 +1,32 @@
+'use strict';
+var {app} = require('electron');
+var path = require('path');
+global.appRoot = path.resolve(__dirname);
+var log = require('./logger.js');
+/*log.transports.file.appName = 'electron-log-test';
+log.transports.file.file = setLogFileName();
+
+function setLogFileName(){
+    var date = new Date();
+    var curTimeStamp = date.getDate() + "_"
+        + (date.getMonth()+1)  + "_"
+        + date.getFullYear() + "_"
+        + date.getHours() + "_"
+        + date.getMinutes() + "_"
+        + date.getSeconds();
+		if (!fs.existsSync(__dirname + "/logs")){
+            fs.mkdirSync(__dirname + "/logs");
+        }
+    return __dirname + "/logs/log_"+curTimeStamp+".txt";
+}*/
+
+app.on('ready', function() {
+	log.info('Hello, info');
+	log.warn('Hello, warn');
+	log.debug('Hello, debug');
+	log.error('Hello, error');
+	if(process.execPath.indexOf('electron') > -1) {
+	  // it handles shutting itself down automatically 
+	  require('electron-local-crash-reporter').start();
+	}
+});
